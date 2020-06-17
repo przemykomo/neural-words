1cd13fc3946e829674e20eb6319924de59e8e53b
Przemek <przemek@przemyk.xyz>
2020-06-17 17:19:10 +0200

    potential different way for converting text into network inputs

--- src/convert.cpp
+++ src/convert.cpp
@@ -4,7 +4,7 @@
 
 std::array<double, networkEntries> convertToInputs(const char* text, int length)  {
     std::array<double, networkEntries> data{};
-    
+
     for(int i{0}; i < length; ++i) {
         if(text[i] == ' ' || text[i] == '-') {
             continue;
@@ -16,6 +16,26 @@ std::array<double, networkEntries> convertToInputs(const char* text, int length)
             std::cout << "Invalid char: " << text[i] << ' ' << static_cast<int>(text[i]) << '\n';
         }
     }
+
+    /*
+    constexpr bitsPerLetter{8};
+
+    for (int i{0}; i < length; ++i) {
+        if (text[i] == ' ' || text[i] == '-') {
+            continue;
+        }
+        
+        std::size_t startIndex{i * bitsPerLetter};
+
+        data[startIndex + 0] = static_cast<double>((text[i] & 0x01) >> 0);
+        data[startIndex + 1] = static_cast<double>((text[i] & 0x02) >> 1);
+        data[startIndex + 2] = static_cast<double>((text[i] & 0x02) >> 2);
+        data[startIndex + 3] = static_cast<double>((text[i] & 0x02) >> 3);
+        data[startIndex + 4] = static_cast<double>((text[i] & 0x02) >> 4);
+        data[startIndex + 5] = static_cast<double>((text[i] & 0x02) >> 5);
+        data[startIndex + 6] = static_cast<double>((text[i] & 0x02) >> 6);
+        data[startIndex + 7] = static_cast<double>((text[i] & 0x02) >> 7);
+    } */
     
     return data; // I'd move it but idk
 }
